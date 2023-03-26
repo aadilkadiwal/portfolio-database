47464ef4e317787219848c35b0f49292a0498a2e
aadilkadiwal <aadilkadiwal4277@gmail.com>
2023-03-26 14:25:52 +0530

    Changes in models

--- src/core/models.py
+++ src/core/models.py
@@ -11,11 +11,19 @@ class AbstractTrack(models.Model):
         abstract = True
 
 
+class AbstractActiveTrack(AbstractTrack):
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
 class User(AbstractTrack):
     first_name = models.CharField(max_length=55)
     surname = models.CharField(max_length=55)
     designation = models.CharField(max_length=55, null=True, blank=True)
     photo = models.ImageField(upload_to="profile/", null=True, blank=True)
+    background_image = models.ImageField(upload_to="background/", null=True, blank=True)
 
     def __str__(self):
         return f"{self.first_name} | {self.surname}" 
--- src/main/models.py
+++ src/main/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from decimal import Decimal
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-from core.models import AbstractTrack
+from core.models import AbstractActiveTrack
 
 
-class Language(AbstractTrack):
+class Language(AbstractActiveTrack):
 
     name = models.CharField(max_length=20)
     rating = models.DecimalField(
@@ -14,62 +14,51 @@ class Language(AbstractTrack):
         default=Decimal(0),
         validators=[MinValueValidator(0), MaxValueValidator(100)]
     )
-    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.name} | {self.rating}"
 
 
-class Skill(AbstractTrack):
+class Skill(AbstractActiveTrack):
     class SkillType(models.TextChoices):
         PROFESSIONAL_SKILL = 'PROFESSIONAL_SKILL'
         SOFTWARE_SKILL = 'SOFTWARE_SKILL'
-        CODE_SKILL = 'CODE_SKILL'
 
     name = models.CharField(max_length=55)
-    skill_type = models.CharField(choices=SkillType.choices, default=SkillType.CODE_SKILL, max_length=18)
-    rating = models.DecimalField(
-        max_digits=3,
-        decimal_places=0,
-        default=Decimal(0),
-        validators=[MinValueValidator(0), MaxValueValidator(100)]
-    )
-    is_active = models.BooleanField(default=True)
+    skill_type = models.CharField(choices=SkillType.choices, default=SkillType.SOFTWARE_SKILL, max_length=18)
 
     def __str__(self):
-        return f"{self.name} | {self.rating}"
+        return f"{self.name} | {self.skill_type}"
 
     
-class Project(AbstractTrack):
+class Project(AbstractActiveTrack):
     name = models.CharField(max_length=55)
     description = models.CharField(max_length=155)
     image = models.ImageField(upload_to='project/')
     url = models.URLField()
-    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.name} | {self.url}"
 
 
-class Experience(AbstractTrack):
+class Experience(AbstractActiveTrack):
     company_name = models.CharField(max_length=55)
     designation = models.CharField(max_length=55)
     description = models.CharField(max_length=255)
     start_date = models.DateField()
     end_date = models.DateField()
-    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.company_name} | {self.designation}"
 
 
-class Education(AbstractTrack):
+class Education(AbstractActiveTrack):
     degree = models.CharField(max_length=100)
     course_name = models.CharField(max_length=155)
-    university = models.CharField(max_length=155)
+    university_name = models.CharField(max_length=155)
+    description = models.CharField(max_length=255)
     start_date = models.DateField()
     end_date = models.DateField()
-    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.degree} | {self.course_name}"
